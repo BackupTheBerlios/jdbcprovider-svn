2de4337b8dfb43a540abd2a477037e063572d2be
mikkle <mikkle@cbff8a5e-db0c-0410-ac51-b7dbe897a8ca>
2009-01-17 00:40:25 +0000

    Tag compat with the older JSPWikis (tested good up to 2.5.112) Trunk will now focus on compatibility with JSPWiki 2.8.1 and forward

---
+++ tags/compat-jspwiki-2.5.x/src/com/forthgo/jspwiki/jdbcprovider/C3P0ConnectionProvider.java
@@ -0,0 +1,103 @@
+/*
+    JDBCProvider - an RDBMS backed page- and attachment provider for
+    JSPWiki.
+ 
+    Copyright (C) 2006-2007 The JDBCProvider development team.
+    
+    The JDBCProvider developer team members are:
+      Xan Gregg
+      Soeren Berg Glasius
+      Mikkel Troest
+      Milt Taylor
+ 
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+ 
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+ 
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.forthgo.jspwiki.jdbcprovider;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.log4j.Logger;
+
+import com.ecyrd.jspwiki.InternalWikiException;
+import com.ecyrd.jspwiki.NoRequiredPropertyException;
+import com.ecyrd.jspwiki.WikiEngine;
+import com.mchange.v2.c3p0.DataSources;
+
+/*
+ * History:
+ * 2007-02-16 MT  Added support for additional driver- and c3p0 properties 
+ * 2007-02-16 MT  Initial version
+ */
+
+/**
+ * @author Mikkel Troest
+ */
+public class C3P0ConnectionProvider extends ConnectionProvider {
+	protected static final Logger log = Logger.getLogger(C3P0ConnectionProvider.class);
+
+	private static final String PREFIX = "c3p0";
+	
+	private String driver;
+	private String url;
+
+	private Properties connectionProperties;
+	private Properties c3p0Properties;
+	
+	private DataSource ds = null;
+	
+  /** Creates a new instance of C3P0ConnectionProvider */
+  public C3P0ConnectionProvider()  {
+  }
+  
+	public void initialize(WikiEngine engine, Properties config)	throws NoRequiredPropertyException {
+    log.debug("Initializing C3P0ConnectionProvider");
+
+    driver = WikiEngine.getRequiredProperty(config, PREFIX+".driverClassName");
+    url = WikiEngine.getRequiredProperty(config, PREFIX+".url");
+    
+    connectionProperties = parseAdditionalProperties(config, DRIVER_PROP_PREFIX);
+    
+    connectionProperties.put("user", WikiEngine.getRequiredProperty(config, PREFIX+".username"));
+    connectionProperties.put("password", WikiEngine.getRequiredProperty(config, PREFIX+".password"));
+    log.debug("driver: "+driver+", url: "+url+", username: "+ connectionProperties.getProperty("user"));
+
+    c3p0Properties = parseAdditionalProperties(config, PREFIX);
+   
+    try {
+      Class.forName(driver);
+
+      ds = DataSources.pooledDataSource(DataSources.unpooledDataSource(url, connectionProperties), c3p0Properties);
+      
+  } catch (SQLException ex) {
+			log.error("Failed to create ConnectionPool", ex);
+			throw new InternalWikiException("SQLException during connection pool creation: " + ex.getMessage());
+		} catch (ClassNotFoundException ex) {
+			log.error("Failed to create ConnectionPool", ex);
+			throw new InternalWikiException("ClassNotFoundException during connection pool creation: " + ex.getMessage());
+		}
+    
+    
+	}
+
+	public Connection getConnection(WikiEngine engine) throws SQLException {
+		return ds.getConnection();
+	}
+
+}
