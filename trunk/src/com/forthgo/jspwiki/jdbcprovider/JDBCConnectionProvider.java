23bcf79d3e2a1d81d390a9b5449f198fc66776e8
mikkle <mikkle@cbff8a5e-db0c-0410-ac51-b7dbe897a8ca>
2009-01-17 19:02:27 +0000

    Build should now work again

--- trunk/src/com/forthgo/jspwiki/jdbcprovider/JDBCConnectionProvider.java
+++ trunk/src/com/forthgo/jspwiki/jdbcprovider/JDBCConnectionProvider.java
@@ -50,8 +50,8 @@ import com.ecyrd.jspwiki.WikiEngine;
  * @author glasius
  */
 public class JDBCConnectionProvider extends ConnectionProvider {
-  d
-		protected static final Logger log = Logger.getLogger(JDBCConnectionProvider.class);
+  
+	protected static final Logger log = Logger.getLogger(JDBCConnectionProvider.class);
 	
 		private static final String PROP_PREFIX = "jdbc";
 	
